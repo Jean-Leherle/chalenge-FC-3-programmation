46fb099cf1731940b1aa69b00c536eb001c18d39
jean-leherle <j.leherle@gmail.com>
2023-05-18 22:35:27 +0200

    remarkable identity is ok

--- java/IdentityQuote.java
+++ java/IdentityQuote.java
@@ -1,3 +1,57 @@
-public class IdentityQuote {
-  
+public class IdentityQuote extends MathQuote {
+  public IdentityQuote() {
+    question = "Indiquer deux nombres dont vous voulez connaître leur identité remarquable : ";
+  }
+
+  protected String calculate(int... parameter) {
+    StringBuilder result = new StringBuilder();
+    int a = parameter[0], b = parameter[1];
+
+    result.append("(")
+        .append(a)
+        .append(" + ")
+        .append(b)
+        .append(")² = ")
+        .append(a)
+        .append("² + 2 x ")
+        .append(a)
+        .append(" x ")
+        .append(b)
+        .append(" + ")
+        .append(b)
+        .append("² = ")
+        .append((a + b) * (a + b))
+        .append("\n");
+    result.append("(")
+        .append(a)
+        .append(" - ")
+        .append(b)
+        .append(")² = ")
+        .append(a)
+        .append("² - 2 x ")
+        .append(a)
+        .append(" x ")
+        .append(b)
+        .append(" + ")
+        .append(b)
+        .append("² = ")
+        .append((a - b) * (a - b))
+        .append("\n");
+    result.append("(")
+        .append(a)
+        .append(" + ")
+        .append(b)
+        .append(") x (")
+        .append(a)
+        .append(" - ")
+        .append(b)
+        .append(") = ")
+        .append(a)
+        .append("² - ")
+        .append(b)
+        .append("² = ")
+        .append((a + b) * (a - b))
+        .append("\n");
+    return result.toString();
+  }
 }
--- java/app.java
+++ java/app.java
@@ -1,16 +1,13 @@
-import java.util.Scanner;
 
-public class app { 
-  
+public class app {
+
   public static void main(String[] args) {
     MultiplicationQuote multiplication = new MultiplicationQuote();
-    //multiplication.quote();
+    // multiplication.quote();
     PythagoreQuote pythagoreQuote = new PythagoreQuote();
-    pythagoreQuote.quote();
-    /*  System.out.println("écriver votre réponse : ");
-    Scanner scanner = new Scanner(System.in);
-    String answer = scanner.next();
-    System.out.println("votre réponse est "+answer);
-    scanner.close(); */
-   }
+    //pythagoreQuote.quote();
+
+    IdentityQuote identityQuote = new IdentityQuote();
+    identityQuote.quote();
+  }
 }
