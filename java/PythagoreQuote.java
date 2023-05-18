3afa0cb9f06d764b16daee9432a6c5c9001cf077
jean-leherle <j.leherle@gmail.com>
2023-05-18 22:02:31 +0200

    multiplication and pythagore without dev print

--- java/PythagoreQuote.java
+++ java/PythagoreQuote.java
@@ -1,16 +1,12 @@
 import java.lang.Math;
+
 public class PythagoreQuote extends MathQuote {
-  PythagoreQuote(){
+  PythagoreQuote() {
     question = "Indiquer les valeurs des côtés connus du triangle :";
   }
-  
+
   protected String calculate(int... parameter) {
-    System.out.println(parameter);
-    System.out.println(parameter[0]);
-    System.out.println(parameter[1]);
-
-
-    Double result = Math.sqrt( Math.pow(parameter[0], 2) + Math.pow(parameter[1],2));
-    return "la longueur du troisiéme coté est "+Double.toString(result) ;
+    Double result = Math.sqrt(Math.pow(parameter[0], 2) + Math.pow(parameter[1], 2));
+    return "la longueur du troisiéme coté est " + Double.toString(result);
   }
 }
